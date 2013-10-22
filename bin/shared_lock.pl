f186c9a9c6c772c36af474f9a27a2289ee7f7af2
Anthony Brummett <abrummet@genome.wustl.edu>
2013-10-22 10:09:55 -0500

    Simple command-line programs to make locks

---
+++ bin/shared_lock.pl
@@ -0,0 +1,16 @@
+use App::Lockd::Client;
+
+my $resource = shift @ARGV;
+
+my $lock_server = App::Lockd::Client->new(host => 'localhost', port => 22334);
+$lock_server || die "can't connect to server";
+
+$|=1;
+print "Locking... ";
+my $lock = $lock_server->lock_shared($resource);
+if ($lock) {
+    print "locked\n";
+    select(undef, undef, undef, undef);
+} else {
+    die "Lock failed";
+}
